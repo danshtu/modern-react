0ce436fa271978cd8464758f3c8711bde548db5f
Daniel Shtunyuk <mrdanilkos@gmail.com>
2025-01-09 22:35:06 -0500

    multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import './App.css'
+import Navbar from "./Navbar.jsx";
+import Home from "./Home.jsx";
 
 function App() {
-  const title = 'welcome to the new blog';
-  const likes = 50;
-  const link = "https://www.google.com";
-
   return (
-    <>
+    <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
