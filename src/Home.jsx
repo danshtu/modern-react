433a24dee4e936a7fb68df2f13f71feeafcadae4
Daniel Shtunyuk <mrdanilkos@gmail.com>
2025-01-23 10:08:03 -0500

    conditional loading message

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,15 +3,20 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => res.json())
-      .then(data => setBlogs(data));
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
