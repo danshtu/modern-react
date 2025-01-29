bbb6a7612bdbbb4214de1fe1fb5b2e655ea9feef
Daniel Shtunyuk <mrdanilkos@gmail.com>
2025-01-28 22:08:56 -0500

    programmatic redirects

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,12 @@
 import {useState} from "react";
+import {useHistory} from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,6 +23,8 @@ const Create = () => {
     }).then(() => {
       console.log('New blog added');
       setIsPending(false);
+      // history.go(-1)
+      history.push('/');
     })
   }
 
