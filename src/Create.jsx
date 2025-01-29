08a6418c3962cbf8a109844652ee693719fadf51
Daniel Shtunyuk <mrdanilkos@gmail.com>
2025-01-28 22:03:34 -0500

    making a post request

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,11 +4,24 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('New blog added');
+      setIsPending(false);
+    })
   }
 
   return (
@@ -24,7 +37,7 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending ? <button>Add Blog</button> : <button disabled>Adding Blog...</button>}
       </form>
     </div>
   )
